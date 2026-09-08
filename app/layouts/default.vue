4eb6139d671f8ccc24cdd6db8434f9a0fba1f54a
DragonSenseiGuy <hey@adityan.dev>
2026-09-07 22:26:38 -0400

    Prune unused Speed Insights dependency

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -47,7 +47,6 @@
 import { ref, nextTick, onMounted, computed } from 'vue';
 import 'highlight.js/styles/github.css';
 import 'highlight.js/styles/github-dark.css';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import { useDark } from "@vueuse/core";
 import { useHead } from '@unhead/vue';
 import { useRoute, useRouter } from 'vue-router';
@@ -430,4 +429,3 @@ function hintFor(id) {
 }
 
 </style>
-
